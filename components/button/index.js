b081080ed134b927487f095863dd6654206a8bc4
NikChao <nik.chaourov@gmail.com>
2019-02-09 16:14:28 +1000

    fix for button

--- components/button/index.js
+++ components/button/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 function Button ({ text, onClick }) {
   return (
-  <button onClick={onClick}>
-    {text || 'Button'}
-  </button>
+    <button onClick={onClick}>
+      {text || 'Button'}
+    </button>
+  );
 }
 
 export { Button };
