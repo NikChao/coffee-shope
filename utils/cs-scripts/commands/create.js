365c56c1d5ca0864d4b7209b1ff83e69379c2dfb
NikChao <nik.chaourov@gmail.com>
2019-02-10 19:55:43 +1000

    feat(cs-scripts): make the command switch meet the pr spec

--- utils/cs-scripts/commands/create.js
+++ utils/cs-scripts/commands/create.js
@@ -1,5 +1,19 @@
-function create () {
-  console.log('creating');
+const chalk = require('chalk');
+
+const clog = (msg, chalkfn) => console.log(chalkfn ? chalkfn(msg) : msg);
+
+function create (moduleType) {
+  switch (moduleType) {
+    case 'component':
+      clog('creating component', chalk.green)
+      return;
+    case 'module':
+      clog('creating module', chalk.green)
+      return;
+    default:
+      clog('invalid module type provided', chalk.red);
+      return;
+  }
 }
 
 module.exports = create;
