a0c119fe9ab43876ac6328d1c7bbe00aefa63e27
NikChao <nik.chaourov@gmail.com>
2019-02-10 23:48:39 +1000

    feat(cs-scripts): get template creation working with storybook config

--- utils/cs-scripts/commands/create.js
+++ utils/cs-scripts/commands/create.js
@@ -5,6 +5,13 @@ const copyDir = require('copy-dir');
 
 const clog = (msg, chalkfn) => console.log(chalkfn ? chalkfn(msg) : msg);
 
+function componentName(name) {
+  return name
+    .split('-')
+    .map(s => s[0].toUpperCase() + s.slice(1))
+    .join('');
+}
+
 function replaceTemplates ({ files=[], name }) {
   files.forEach(file => {
     fs.readFile(file, 'utf8', function (err,data) {
@@ -12,18 +19,51 @@ function replaceTemplates ({ files=[], name }) {
         return console.log(err);
       }
 
-      const componentName =
-        name.split('-').map(s => s[0].toUpperCase() + s.slice(1)).join('')
+      const component = componentName(name);
 
       const result = data
         .replace(/{{TEMPLATE_NAME}}/g, name)
-        .replace(/{{COMPONENT_NAME}}/g, componentName);
+        .replace(/{{COMPONENT_NAME}}/g, component);
   
       fs.writeFile(file, result, 'utf8', function (err) {
         if (err) return console.log(err);
       });
     });
-  })
+  });
+}
+
+function addToStorybook(name) {
+  const storybookConfig = path.resolve() + `/.storybook/config.js`;
+  console.log('here');
+
+  if (!fs.existsSync(storybookConfig)) {
+    clog('Storybook config does not exist', chalk.yellow);
+    return;
+  }
+
+  console.log('here');
+  fs.readFile(storybookConfig, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    const statement = `require('../components/${name}/stories/index.js');`
+
+    if (data.includes(statement)) {
+      clog('story already exists for this component', chalk.yellow);
+      return;
+    }
+
+    const result = data
+      .replace(
+        'function loadStories() {\n',
+        `function loadStories() {\n  ${statement}\n`
+      );
+
+    fs.writeFile(storybookConfig, result, 'utf8', function (err) {
+      if (err) return console.log(err);
+    });
+  });
 }
 
 function createComponent(name) {
@@ -38,13 +78,18 @@ function createComponent(name) {
     const src = __dirname + '/../templates/component';
     const dest = path.resolve() + `/components/${name}`;
     copyDir.sync(src, dest);
-
+    
+    clog(`Created template!`, chalk.blue);
+    
     replaceTemplates({
-      files: ['package.json', 'index.js', '.stories/index.js'].map(f => `${dest}/${f}`),
+      files: ['package.json', 'index.js', 'stories/index.js'].map(f => `${dest}/${f}`),
       name
     });
 
-    clog(`Created template!`)
+    clog(`Adding to storybook!`, chalk.blue);
+
+    addToStorybook(name)
+
     clog(`Created ${name}!`, chalk.green);
   } catch (err) {
     clog(`Failed to create ${name}`, chalk.red);
