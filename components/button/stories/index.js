745cea9cf4ac3e14618b94087ed9745b30c01e18
NikChao <nik.chaourov@gmail.com>
2019-02-12 23:37:22 +1000

    fix(button): Make button story onClick not an alert

--- components/button/stories/index.js
+++ components/button/stories/index.js
@@ -2,22 +2,24 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '../index.js';
 
+const click = () => console.log('click!');
+
 storiesOf('Button', module)
   .add('default', () => (
     <div>
-      <Button onClick={window.alert} >Click Me</Button>
-      <Button onClick={window.alert} disabled >Click Me</Button>
+      <Button onClick={click} >Click Me</Button>
+      <Button onClick={click} disabled >Click Me</Button>
     </div>
   ))
   .add('positive', () => (
     <div>
-      <Button onClick={window.alert} positive>Click Me</Button>
-      <Button onClick={window.alert} positive disabled >Click Me</Button>
+      <Button onClick={click} positive>Click Me</Button>
+      <Button onClick={click} positive disabled >Click Me</Button>
     </div>
   ))
   .add('text only', () => (
     <div>
-      <Button onClick={window.alert} textOnly>Click Me</Button>
-      <Button onClick={window.alert} textOnly disabled >Click Me</Button>
+      <Button onClick={click} textOnly>Click Me</Button>
+      <Button onClick={click} textOnly disabled >Click Me</Button>
     </div>
   ));
