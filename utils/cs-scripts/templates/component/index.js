bba81854dc40205689aa3d31228b84ee6f36c087
NikChao <nik.chaourov@gmail.com>
2019-02-16 00:50:44 +1000

    add scss module to cs-scripts template

--- utils/cs-scripts/templates/component/index.js
+++ utils/cs-scripts/templates/component/index.js
@@ -1,8 +1,9 @@
 import React, { PureComponent } from 'react';
+import styles from './styles.scss';
 
 class {{COMPONENT_NAME}} extends PureComponent {
   render () {
-    return <span>Hello, {{TEMPLATE_NAME}}</span>;
+    return <span className={styles.container}>Hello, {{TEMPLATE_NAME}}</span>;
   }
 }
 
