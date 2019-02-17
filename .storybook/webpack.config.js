aeabf251a56ca0cdfc065cb74b630f127a41a378
NikChao <nik.chaourov@gmail.com>
2019-02-18 09:31:12 +1000

    Add ts loader to storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,6 +4,47 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(ts|tsx)$/,
+        include: path.resolve() + '/',
+        exclude: path.resolve(__dirname, '/node_modules'),
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                useBuiltIns: 'usage',
+                modules: 'commonjs',
+                exclude: ['es6.promise']
+              }
+            ],
+            '@babel/preset-react',
+            '@babel/preset-typescript'
+          ],
+          plugins: [
+            [
+              '@babel/plugin-proposal-decorators',
+              {
+                legacy: true
+              }
+            ],
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-proposal-json-strings',
+            '@babel/plugin-proposal-object-rest-spread',
+            '@babel/plugin-syntax-import-meta',
+            [
+              '@babel/plugin-transform-runtime',
+              {
+                helpers: true,
+                corejs: 2,
+                regenerator: true
+              }
+            ],
+            '@babel/plugin-syntax-dynamic-import'
+          ]
+	}
+      },
+      {
         test: /\.scss$/,
         loaders: [
           {
