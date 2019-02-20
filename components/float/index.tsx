4e254e6588125c288d987da5b04da58214a45fd7
NikChao <nik.chaourov@gmail.com>
2019-02-20 23:31:13 +1000

    feat(float): Add prototype float

--- components/float/index.tsx
+++ components/float/index.tsx
@@ -1,8 +1,15 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, ReactElement } from 'react';
 import styles from './styles.scss';
+const { Ripple } = require('@coffee-shope/ripple');
 
 interface Props {
-
+  onClick: (e: any) => any;
+  mini?: boolean;
+  circular?: boolean;
+  disabled?: boolean;
+  loading?: boolean;
+  className?: string;
+  text?: string;
 }
 
 interface State {
@@ -10,8 +17,33 @@ interface State {
 }
 
 class Float extends PureComponent<Props, State> {
+  className = [
+    styles.float,
+    this.props.disabled && styles.disabled,
+    this.props.mini && styles.mini,
+    this.props.loading && styles.loading,
+    this.props.circular && styles.circular,
+    this.props.className,
+  ].join(' ')
+
   render () {
-    return <span className={styles.container}>Hello, float</span>;
+    const {
+      onClick,
+      className,
+      disabled,
+      text
+    } = this.props;
+
+    return (
+      <Ripple>
+        {({ ripple, mergeEventHandlers }) => (
+          <button {...mergeEventHandlers({ onClick })} className={this.className} disabled={disabled}>
+            {!disabled && ripple}
+            {text}
+          </button>
+        )}
+      </Ripple>
+    );
   }
 }
 
--- components/float/stories/index.tsx
+++ components/float/stories/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Float } from '../index.tsx';
+import { Float } from '../index';
+
+function click () {
+  console.log('click');
+}
 
 storiesOf('Float', module)
   .add('default', () => (
-    <Float />
+    <Float onClick={click} text="This is a floating button" />
+  ))
+  .add('disabled', () => (
+    <Float onClick={click} text="nuh-uh" disabled />
   ));
