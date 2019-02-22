e0f43fe21867468a812929d8d386410eadc0b93b
NikChao <nik.chaourov@gmail.com>
2019-02-22 19:32:05 +1000

    feat(float): add more states

--- components/float/index.tsx
+++ components/float/index.tsx
@@ -1,6 +1,7 @@
 import React, { PureComponent, ReactElement } from 'react';
 import styles from './styles.scss';
-const { Ripple } = require('@coffee-shope/ripple');
+import { Ripple } from '@coffee-shope/ripple'; 
+import { LoadingSpinner } from './loading-circle';
 
 interface Props {
   onClick: (e: any) => any;
@@ -9,7 +10,9 @@ interface Props {
   disabled?: boolean;
   loading?: boolean;
   className?: string;
+  width?: string;
   text?: string;
+  bottomRight?: boolean;
 }
 
 interface State {
@@ -22,27 +25,53 @@ class Float extends PureComponent<Props, State> {
     this.props.disabled && styles.disabled,
     this.props.mini && styles.mini,
     this.props.loading && styles.loading,
-    this.props.circular && styles.circular,
     this.props.className,
   ].join(' ')
+
+  width = this.props.circular
+    ? undefined
+    : this.props.width || 'min-content';
+
+  padding = this.props.circular
+    ? this.props.loading
+    ? '5px'
+    : undefined
+    : undefined;
+
+  positioning: React.CSSProperties = this.props.bottomRight
+    ? { position: 'relative', width: '100%', height: '100%', paddingLeft: '80px', paddingTop: '80px' }
+    : {};
 
   render () {
     const {
       onClick,
-      className,
       disabled,
+      loading,
       text
     } = this.props;
 
     return (
+      <div style={this.positioning}>
       <Ripple>
         {({ ripple, mergeEventHandlers }) => (
-          <button {...mergeEventHandlers({ onClick })} className={this.className} disabled={disabled}>
-            {!disabled && ripple}
+          <button
+            {...mergeEventHandlers({ onClick })}
+            className={this.className} disabled={disabled}
+            style={{
+              width: this.width,
+              padding: this.padding,
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            {!disabled && !loading && ripple}
+            {loading && <LoadingSpinner />}
             {text}
           </button>
         )}
       </Ripple>
+      </div>
     );
   }
 }
--- components/float/stories/index.tsx
+++ components/float/stories/index.tsx
@@ -6,9 +6,34 @@ function click () {
   console.log('click');
 }
 
+function lotsOfText () {
+  return [...Array(128)].map(() => 'asdf').join(' ');
+}
+
 storiesOf('Float', module)
   .add('default', () => (
-    <Float onClick={click} text="This is a floating button" />
+    <Float width="200px" onClick={click} text="This is a floating button" />
+  ))
+  .add('circular', () => (
+    <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+      <div style={{ padding: '20px' }}>
+        <Float circular onClick={click} text="+" />    
+      </div>
+      <div style={{ padding: '20px' }}>
+        <Float circular loading onClick={click}/>    
+      </div>
+      <div style={{ padding: '20px' }}>
+        <Float circular disabled onClick={click} text="+" />    
+      </div>
+    </div>
+  ))
+  .add('absolute', () => (
+    <div style={{ width: '300px', height: '200px', overflow: 'auto', position: 'absolute' }}>
+      <span style={{ width: '100%', display: 'flex', flexWrap: 'wrap', position: 'absolute' }}>
+        {lotsOfText()}
+      </span>
+      <Float bottomRight circular onClick={click} text="+" />
+    </div>
   ))
   .add('disabled', () => (
     <Float onClick={click} text="nuh-uh" disabled />
