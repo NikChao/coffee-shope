45e07b5d51338998bb0cf40d749588bbee3c6339
NikChao <nik.chaourov@gmail.com>
2019-02-27 10:26:23 +1000

    feat(float): Lorem ipsum into story + add decorators + skipLibCheck to config

--- components/float/stories/index.tsx
+++ components/float/stories/index.tsx
@@ -7,7 +7,9 @@ function click () {
 }
 
 function lotsOfText () {
-  return [...Array(128)].map(() => 'asdf').join(' ');
+  return `
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  `;
 }
 
 storiesOf('Float', module)
