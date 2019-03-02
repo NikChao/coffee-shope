d0002cacd0657f3ed93d7ef8aad91fc997fb126d
NikChao <nik.chaourov@gmail.com>
2019-03-02 23:13:39 +1000

    temp fix storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,9 +2,9 @@ import { configure } from '@storybook/react';
 
 function loadStories() {
   require('../components/animator/stories/index.tsx');
-  require('../components/float/stories/index.tsx');
+  // require('../components/float/stories/index.tsx');
   require('../components/grid/stories/index.tsx');
-  require('../components/ripple/stories/index.tsx');
+  // require('../components/ripple/stories/index.tsx');
 
   require('../components/checkbox/stories/index.js');
   require('../components/field/stories/index.js');
