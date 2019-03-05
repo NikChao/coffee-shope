2c93a5ae41480ce1c30b854eba17fbd1005ef7b0
NikChao <nik.chaourov@gmail.com>
2019-03-05 15:49:08 +1000

    fix: fix babel runtime issue

--- utils/barista/commands/build/configs/babel.config.js
+++ utils/barista/commands/build/configs/babel.config.js
@@ -39,7 +39,7 @@ module.exports = function (moduleType) {
 
   return babel({
     runtimeHelpers: true,
-    exclude: 'node_modules/**',
+    exclude: /node_modules/,
     presets,
     plugins
   })
