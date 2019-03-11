99c7c9bb8904c3d73ea096ba359a2e2c31aeddee
NikChao <nik.chaourov@gmail.com>
2019-03-11 23:16:07 +1000

    Update ripple in float

--- components/float/src/index.tsx
+++ components/float/src/index.tsx
@@ -1,6 +1,6 @@
 import React, { PureComponent, ReactElement } from 'react';
 import styles from './styles.scss';
-import { Ripple } from '../../ripple/src'; 
+import { Ripple } from '@coffee-shope/ripple'; 
 import { LoadingSpinner } from './loading-circle';
 
 interface Props {
