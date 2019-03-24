e49d9fb5fbd2b4327233156e39fda3af0ad16464
NikChao <nik.chaourov@gmail.com>
2019-03-24 13:38:21 +1000

    Update coffee-shope ripple in float and make it greeeen

--- components/float/src/index.tsx
+++ components/float/src/index.tsx
@@ -1,6 +1,6 @@
 import React, { PureComponent, ReactElement } from 'react';
 // import styles from './styles.scss';
-import { Ripple } from './ripple'; 
+import { Ripple } from '@coffee-shope/ripple'; 
 import { LoadingSpinner } from './loading-circle';
 import styled from '@emotion/styled';
 
