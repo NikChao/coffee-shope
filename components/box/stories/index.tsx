cd1c58a042f41b1cf0185420223998b584a87b9f
jay-almaraz <jay.amgrant@gmail.com>
2020-04-27 13:38:52 +1000

    use CoffeeShopeThemeProvider in all active stories

--- components/box/stories/index.tsx
+++ components/box/stories/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { CoffeeShopeThemeProvider } from '@coffee-shope/theme-provider';
 import { Box } from '../src/index';
 
-storiesOf('Box', module)
-.add('default', () => (
-  <Box p={10} pl={20} pt={20} pb={100} border='1px solid black'>
-    <pre>This is a layout with</pre>
-    <Box>
-      <pre>{'p={10} pl={20} pt={20} pb={100}'}</pre>
+storiesOf('Box', module).add('default', () => (
+  <CoffeeShopeThemeProvider>
+    <Box p={10} pl={20} pt={20} pb={100} border="1px solid black">
+      <pre>This is a layout with</pre>
+      <Box>
+        <pre>{'p={10} pl={20} pt={20} pb={100}'}</pre>
+      </Box>
     </Box>
-  </Box>
+  </CoffeeShopeThemeProvider>
 ));
--- components/checkbox/stories/index.js
+++ components/checkbox/stories/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { CoffeeShopeThemeProvider } from '@coffee-shope/theme-provider';
 import { ControlledCheckbox, UncontrolledCheckbox } from '../src';
 
 function onChange(e) {
   console.log(e.target.value ? 'checked' : 'not-checked');
 }
-storiesOf('Checkbox', module).add('default', () => <UncontrolledCheckbox label="Remember me" onChange={onChange} />);
+storiesOf('Checkbox', module).add('default', () => (
+  <CoffeeShopeThemeProvider>
+    <UncontrolledCheckbox label="Remember me" onChange={onChange} />
+  </CoffeeShopeThemeProvider>
+));
